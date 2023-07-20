92b79286d59b0ae523dcdb5e695a4b7aaa1da709
Marco <marcoagudo21@gmail.com>
2023-07-20 00:11:41 -0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,47 @@ function App() {
   const imgUrl = "https://cataas.com";
   const [fact, setFact] = useState();
   const [img, setImg] = useState();
-
+  const [reload, setReaload] = useState(true)
+  const handleButton = () => {
+    setReaload(!reload)
+  }
   useEffect(() => {
-    if (!fact) {
+    
       fetch(factUrl)
         .then((res) => res.json())
         .then((data) => {
           let { fact } = data;
           setFact(fact.split(" ").splice(0, 3).join(" "));
         });
-    }
-  }, []);
+    
+  }, [reload]);
 
   useEffect(() => {
-    if (fact) {
-      fetch(`https://cataas.com/cat/says/${fact}?json=true`)
+    if (!fact) {
+      return
+    }
+    fetch(`https://cataas.com/cat/says/${fact}?json=true`)
         .then((res) => res.json())
         .then((data) => {
           let { url } = data;
           setImg(url);
         });
-    }
   }, [fact]);
+
+
+
 
   return (
     <>
       <main className="container">
+        
         <section>
-          {fact && <h1>{fact}</h1>}
-          {img && <img src={`${imgUrl}${img}`} alt="" />}
-        </section>
+        {fact && <h1>{fact}</h1>}
+        {img && <img src={`${imgUrl}${img}`} alt="" />}
+      </section>
+        
+        <button onClick={handleButton}>New Fact</button>
+        
       </main>
     </>
   );
